7de1b02bab790c94aeca60d2901b8dc32d0492ae
Unknown <aspruce@mail.csuchico.edu>
2017-04-06 12:34:32 -0700

    Finished

--- ghostwriter.js
+++ ghostwriter.js
@@ -1,6 +1,17 @@
 function writeTweet()
 {
-  
+  var randIndex;
+  var sentence;
+  var intro = ["What really grinds my gears is eating","Do you want to build", "Have you ever eaten a", "Have you ever noticed"];
+  var body = [" pancakes", " muffins", " banana peppers" , " snowmen", " quiche"];
+  var end = [" while skating", " while surfing"];
+  randIndex = randomUpTo((intro.length-1));
+  sentence = intro[randIndex];
+  randIndex = randomUpTo((body.length-1));
+  sentence = sentence + body[randIndex];
+  randIndex = randomUpTo((end.length-1));
+  sentence = sentence + end[randIndex];
+  document.getElementById("tweet").innerHTML = sentence;
 }
 
 //Generates a random whole number between 0 and max (inclusive)
